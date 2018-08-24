2ed5f3c64dbab716ec25af49dd7a3d77e91f3326
Andy Camara <acamara86@gmail.com>
2018-08-23 22:13:44 -0400

    add logo and slides script

--- get_slides.sh
+++ get_slides.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget 'https://conferences.oreilly.com/oscon/oscon-or/public/schedule/proceedings'  && cat proceedings | grep '.ppt\|.pptx\|.pdf\|.zip' | cut -d'"' -f2 | while read line; do wget "$line"; done < "${1:-/dev/stdin}"
+wget 'https://conferences.oreilly.com/oscon/oscon-or/public/schedule/proceedings' && cat proceedings | grep '.ppt\|.pptx\|.pdf\|.zip' | cut -d'"' -f2 | while read line; do wget "$line"; done < "${1:-/dev/stdin}"
